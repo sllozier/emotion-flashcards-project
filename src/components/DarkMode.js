ecc2df43772a891dca9c1bc1be87d31fdc922204
Sarah Lozier <sarah.lozier@gmail.com>
2022-09-29 17:55:31 -0400

    changed dark mode

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -3,8 +3,7 @@ import { BsMoonStarsFill, BsFillSunFill } from "react-icons/bs";
 import { useDispatch, useSelector } from 'react-redux';
 import { cardsReducer } from '../store/cardsReducer';
 import { toggleDarkMode } from '../store/darkModeReducer';
-import darkBackground from '/home/sarah/Fullstack_Stuff/SeniorPhase/AsyncProject/emotion-flashcards-project/public/darkbackground.png';
-import lightBackground from '/home/sarah/Fullstack_Stuff/SeniorPhase/AsyncProject/emotion-flashcards-project/public/lightbackground.png';
+
 
 
 const DarkMode = () => {
@@ -20,7 +19,7 @@ const DarkMode = () => {
     };
 
     useEffect(() => {
-        document.body.style.backgroundImage = isDarkMode ? `url(${darkBackground})` : `url(${lightBackground})`;
+        document.body.style.backgroundColor = isDarkMode ? `#292c35` : `#fff`;
         document.body.style.color = isDarkMode ? '#fff' : '#292c35';
     }, [isDarkMode]);
 
