8ff9dd2e6fd27c44f5534a46a895f7d9238b902b
Sarah Lozier <sarah.lozier@gmail.com>
2022-09-29 16:21:07 -0400

    change webpack

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,6 @@ module.exports = {
           presets: ["@babel/preset-env", "@babel/preset-react"],
         },
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
     ],
   },
 };
